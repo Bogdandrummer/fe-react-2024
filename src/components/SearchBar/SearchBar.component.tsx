3b5d3d2eca3b7639118700d06757dda7cb972d32
Bogdan <104357137+Bogdandrummer@users.noreply.github.com>
2024-06-04 12:06:31 +0300

    Second commit

--- src/components/SearchBar/SearchBar.component.tsx
+++ src/components/SearchBar/SearchBar.component.tsx
@@ -1,8 +1,30 @@
+import Search from '@/assets/icons/search.svg?react';
+import { Button } from '@/components/button/Button.tsx';
+
 import styles from './SearchBar.module.css';
 export const SearchBar = () => (
     <>
         <form className={styles.search__bar}>
-            <input className={styles.search__field} type="text" placeholder="Search..." />
+            <label className={styles.input__box}>
+                <input className={styles.search__field} id={styles.search__field} type="text" placeholder="Search..." />
+                <Search className={styles.search__icon} />
+            </label>
+            <div className={styles.type__goods}>
+                <Button className={styles.search__button}>Electronics</Button>
+                <Button className={styles.search__button}>Shoes</Button>
+                <Button className={styles.search__button}>Clothes</Button>
+            </div>
+            <div className={styles.sort__filter}>
+                <span className={styles.sort__title}>Sort by:</span>
+                <div>
+                    <ul className={styles.sort__list}>
+                        <li className={styles.list__item}>Price (High - Low)</li>
+                        <li className={styles.list__item}>Price (Low - High)</li>
+                        <li className={styles.list__item}>Newest</li>
+                        <li className={styles.list__item}>Oldest</li>
+                    </ul>
+                </div>
+            </div>
         </form>
     </>
 );
--- src/main.tsx
+++ src/main.tsx
@@ -5,12 +5,14 @@ import { Footer } from '@/components/footer/Footer.component.tsx';
 import { Header } from '@/components/header/Header.component.tsx';
 // import About from './components/about/About.tsx';
 import { ProductsList } from '@/components/ProductsList/ProductsList.component.tsx';
+import { SearchBar } from '@/components/SearchBar/SearchBar.component.tsx';
 
 import './index.css';
 
 ReactDOM.createRoot(document.querySelector('#root')!).render(
     <React.StrictMode>
         <Header />
+        <SearchBar />
         <ProductsList />
         {/* <About />*/}
         <Footer />
